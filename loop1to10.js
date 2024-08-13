6e71811510ca8bba81061a3c3a5f840381e0ec71
vijayprasanth1234 <vijayprasanth2999@gmail.com>
2024-08-14 00:13:47 +0530

    vv

---
+++ loop1to10.js
@@ -0,0 +1,18 @@
+
+
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+
+for(i=10;i>=1;i--){
+    console.log(i)
+}
+
+
+
+let x=10
+let y=20
+let z=30
+let largest=((x>y) && x>z)?"x is greater":(y>z)?"y is greater":"z is greater"
+console.log(largest)
